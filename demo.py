4852280a4607ee478414b4c67e5b6f2b27fb9306
Francesco Uliana <francesco@uliana.it>
2013-04-17 18:20:56 +0000

    added useful comments

--- demo.py
+++ demo.py
@@ -6,7 +6,10 @@ import props
 auth = tweepy.OAuthHandler(props.consumer_key, props.consumer_secret)
 auth.set_access_token(props.access_token, props.access_token_secret)
 
+#twitter account to monitor
 account = "romamobilita"
+
+#topics of interest
 topics = ["metro", "scioper"]
 hours=1
 
@@ -14,16 +17,19 @@ api = tweepy.API(auth)
 
 user = api.get_user(account)
 
+# filter tweets of interest
 def isInteresting(s):
   flag = False
   for topic in topics:
     flag = flag or s.find(topic) >= 0
   return flag
 
+# filter recent tweets
 def recent(el):
     c = datetime.datetime.now() - el.created_at
     return c.seconds < 60*(60*hours+5) and isInteresting(el.text.lower())
 
+# send notification using boxcat
 def send_notice(site, code):
     api_url = 'http://boxcar.io/devices/providers/%s/notifications' % props.BOXCAR_API_KEY
     print api_url
